a14e06a652e1328d5acc083e5089f33fbff506c7
Md Razzak Hossain Fahim <262-16-115@diu.edu.bd>
2026-09-26 00:23:20 +0600

    Update 1. Insert in array.c

--- 1. Insert in array.c
+++ 1. Insert in array.c
@@ -1,43 +1,58 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main(){
-    int n, i, v, j;
+int main() {
+    int n, i, v, j, choice;
+
     printf("Enter the size of array: ");
     scanf("%d", &n);
-    int a[n];
 
-    for(int i = 0; i < n; i++){
+    int a[100];
+
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    for(int i = 0; i < n; i++){
+    printf("\nOriginal array: ");
+    for (i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
-    printf("\nIn which position do you want to add a new value: ");
-    scanf("%d", &i);
-    printf("\nEnter the value: ");
-    scanf("%d", &v);
 
-    if(i >= n){
-        if(i > n)
-            printf("\nError!");
-        else{
-            a[n] = v;
-            n = n + 1;
+    do {
+        printf("\n\nIn which position do you want to add a new value: ");
+        scanf("%d", &i);
+
+        printf("Enter the value: ");
+        scanf("%d", &v);
+
+        if (i < 0 || i > n) {
+            printf("Error! Invalid position.");
         }
-    }else{
-        for(j = n; j > i; j--)
-            a[j] = a[j - 1];
+        else {
+            for (j = n; j > i; j--) {
+                a[j] = a[j - 1];
+            }
 
-        a[i] = v;
-        n = n + 1;
-    }
+            a[i] = v;
+            n++;
 
+            printf("Array after insertion: ");
+            for (j = 0; j < n; j++) {
+                printf("%d ", a[j]);
+            }
+        }
 
-    for(int i = 0; i < n; i++){
+        printf("\n\nDo you want to insert another element? (1 = Yes, 0 = No): ");
+        scanf("%d", &choice);
+
+    } while (choice == 1);
+
+    printf("\nFinal array: ");
+    for (i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
-    getche();
-}
 
+    getche();
+    return 0;
+}
